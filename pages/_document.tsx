3adc3a351cee99c612c79315aef85312873a9b0f
Ali Emir Sen <senaliemir@gmail.com>
2021-01-10 17:44:57 +0300

    add favicons and webmanifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,25 @@ class MyDocument extends Document {
             type="font/ttf"
             crossOrigin="anonymous"
           />
-          <link href="/favicon.ico" rel="shortcut icon" />
+          <link href="/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/site.webmanifest" rel="manifest" />
+          <link
+            href="/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta content="#ffffff" name="theme-color" />
           <meta content="#ffffff" name="msapplication-TileColor" />
