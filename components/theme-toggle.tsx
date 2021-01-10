423674bddde647c74c701c0ec2f19331231ab09f
Ali Emir Sen <senaliemir@gmail.com>
2021-01-10 21:56:59 +0300

    add theme toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -4,23 +4,29 @@ import { useTheme } from "next-themes"
 export const ThemeToggle: React.FC = () => {
   const { theme, setTheme } = useTheme()
   const [isMounted, setMounted] = React.useState(false)
+  const [s, setSwitch] = React.useState(theme)
 
   React.useEffect(() => {
     setMounted(true)
   }, [])
 
+  React.useEffect(() => {
+    setTheme(s)
+  }, [s, setTheme])
+
   const toggleTheme = (): void => {
     if (isMounted) {
-      setTheme(theme === "light" ? "dark" : "light")
+      setSwitch(s === "light" ? "dark" : "light")
     }
   }
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="bg-white dark:bg-black text-black dark:text-white"
-    >
-      Toggle theme
+    <button onClick={toggleTheme} className="focus:outline-none">
+      {s === "light" ? (
+        <img src="icons/moon.svg" alt="switch to dark mode" />
+      ) : (
+        <img src="icons/sun.svg" alt="switch to dark mode" />
+      )}
     </button>
   )
 }
