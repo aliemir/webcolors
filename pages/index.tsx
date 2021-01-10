24378926c969dce19921831e4b002548799f12c3
Ali Emir Sen <senaliemir@gmail.com>
2021-01-10 16:51:42 +0300

    type index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-const Home = () => {
+import { NextPage } from "next"
+
+const Home: NextPage = () => {
   return (
-    <div className='p-4 shadow rounded bg-white'>
-      <h1 className='text-purple-500 leading-normal'>Next.js</h1>
-      <p className='text-gray-500'>with Tailwind CSS</p>
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple-500 leading-normal">Next.js</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
     </div>
   )
 }
